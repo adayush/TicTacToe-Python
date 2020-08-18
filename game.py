3e590d371513d538257013ce6188e11424b9d97c
adayush <adayush0@gmail.com>
2020-08-18 18:37:01 +0530

    Created the basic layout

---
+++ game.py
@@ -0,0 +1,14 @@
+print("———— Welcome to Tic-Tac-Toe made by Ayush ————\n\n")
+
+moves = {'7': " ", '8': " ", '9': " ",
+         '4': " ", '5': " ", '6': " ",
+         '1': " ", '2': " ", '3': " "}
+
+print(" %c | %c | %c" % (moves['7'], moves['8'], moves['9']))
+print("———|———|———")
+print(" %c | %c | %c" % (moves['4'], moves['5'], moves['6']))
+print("———|———|———")
+print(" %c | %c | %c" % (moves['1'], moves['2'], moves['3']))
+
+print("\n\n")
+
