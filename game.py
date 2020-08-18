9f9853719c680de273f0c678c96c0d561b785c9b
adayush <adayush0@gmail.com>
2020-08-18 21:27:49 +0530

    The Game works perfectly

--- game.py
+++ game.py
@@ -1,13 +1,13 @@
 def check():
-    # horizontal checking
-    for x in (7, 4, 1):
-        if moves[str(x)] == moves[str(x+1)] == moves[str(x+2)]:
+    # horizontal checking - WORKS
+    for x in (1, 4, 7):
+        if moves[str(x)] == moves[str(x+1)] == moves[str(x+2)] == player:
             return moves[str(x)]
     
     i = 0
     while(i < 3):
-        if moves[str(7 + x)] == moves[str(4 + x)] == moves[str(1 + x)]:
-            return moves[str(7)]
+        if moves[str(7 + i)] == moves[str(4 + i)] == moves[str(1 + i)]:
+            return moves[str(7 + i)]
         i += 1
 
     if moves['7'] == moves['5'] == moves['3']:
@@ -32,14 +32,14 @@ while(turns<9):
     print("———|———|———")
     print(" %c | %c | %c" % (moves['1'], moves['2'], moves['3']))
 
-    winner = check()
-    print("winner is|" + winner + "|")
-    if winner == 'X':
-        print("Game Over\n" + winner + " is the winner!")
-        break
-    elif winner == 'O':
-        print("Game Over\n" + winner + " is the winner!")
-        break
+    if turns > 3:
+        winner = check()
+        if winner == 'X':
+            print("Game Over\n" + winner + " is the winner!")
+            break
+        elif winner == 'O':
+            print("Game Over\n" + winner + " is the winner!")
+            break
 
     player = 'X' if (turns%2 == 0) else 'O'
 
