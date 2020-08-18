a5a2635286a61d07fcf1a326197b55764ecdc9ca
adayush <adayush0@gmail.com>
2020-08-18 20:31:48 +0530

    Game working now

--- game.py
+++ game.py
@@ -1,4 +1,23 @@
-print("———— Welcome to Tic-Tac-Toe made by Ayush ————\n\n")
+def check():
+    # horizontal checking
+    for x in (7, 4, 1):
+        if moves[str(x)] == moves[str(x+1)] == moves[str(x+2)]:
+            return moves[str(x)]
+    
+    i = 0
+    while(i < 3):
+        if moves[str(7 + x)] == moves[str(4 + x)] == moves[str(1 + x)]:
+            return moves[str(7)]
+        i += 1
+
+    if moves['7'] == moves['5'] == moves['3']:
+        return moves['7']
+    elif moves['1'] == moves['5'] == moves['9']:
+        return moves['1']
+    
+    return ' '
+
+print("———— Welcome to Tic-Tac-Toe made by Us ————\n\n")
 
 moves = {'7': " ", '8': " ", '9': " ",
          '4': " ", '5': " ", '6': " ",
@@ -13,6 +32,15 @@ while(turns<9):
     print("———|———|———")
     print(" %c | %c | %c" % (moves['1'], moves['2'], moves['3']))
 
+    winner = check()
+    print("winner is|" + winner + "|")
+    if winner == 'X':
+        print("Game Over\n" + winner + " is the winner!")
+        break
+    elif winner == 'O':
+        print("Game Over\n" + winner + " is the winner!")
+        break
+
     player = 'X' if (turns%2 == 0) else 'O'
 
     print(player + "'s turn")
@@ -21,4 +49,6 @@ while(turns<9):
     moves[move] = player
 
     turns += 1
-    print("\n")
+
+if turns == 9:
+    print("It was a Tie. Well Played :D")
