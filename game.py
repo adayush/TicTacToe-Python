3186300ac92a88858158d2d3c7eb23002e5748da
adayush <adayush0@gmail.com>
2020-08-18 18:57:28 +0530

    Game is now playable but no one wins

--- game.py
+++ game.py
@@ -4,11 +4,21 @@ moves = {'7': " ", '8': " ", '9': " ",
          '4': " ", '5': " ", '6': " ",
          '1': " ", '2': " ", '3': " "}
 
-print(" %c | %c | %c" % (moves['7'], moves['8'], moves['9']))
-print("———|———|———")
-print(" %c | %c | %c" % (moves['4'], moves['5'], moves['6']))
-print("———|———|———")
-print(" %c | %c | %c" % (moves['1'], moves['2'], moves['3']))
+turns = 0
 
-print("\n\n")
+while(turns<9):
+    print(" %c | %c | %c" % (moves['7'], moves['8'], moves['9']))
+    print("———|———|———")
+    print(" %c | %c | %c" % (moves['4'], moves['5'], moves['6']))
+    print("———|———|———")
+    print(" %c | %c | %c" % (moves['1'], moves['2'], moves['3']))
 
+    player = 'X' if (turns%2 == 0) else 'O'
+
+    print(player + "'s turn")
+    
+    move = input()
+    moves[move] = player
+
+    turns += 1
+    print("\n")
